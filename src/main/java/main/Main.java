b2d595e22bf8a4cf9c67d0dede0e14629bcf32de
Dennis <joklin@live.dk>
2018-11-22 12:13:32 +0100

    minor main change

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -22,7 +22,7 @@ public class Main {
         //UserFacade
 //        UserFacade facade = new UserFacade();
 //        Role userRole = new Role("user");
-//        User user = new User("mainF", "mainL", "main@main.main", "hellofrommain", "123456789", "CityHus", "mainstreet 1.tv", "2100");
+//        User user = new User("mainF", "mainL", "main@main.main", "hellofrommain", "123456789", "CityHus", "mainstreet 1.tv", 2100);
 //        user.addRole(userRole);
 //        System.out.println("CreateUser: " + facade.createUser(user));
 
@@ -30,7 +30,7 @@ public class Main {
         //CandyFacade
 //        CandyFacade cfacade = new CandyFacade();
 //        CandyType type = new CandyType("mainJordbær");
-//        System.out.println("CreateCandy: " + cfacade.addShop(type));
+//        System.out.println("CreateCandy: " + cfacade.addCandy(type));
 
 
         //----------------------------------------------------------------------
@@ -50,12 +50,12 @@ public class Main {
         
         //----------------------------------------------------------------------
         //ShopFacade
-        ShopFacade sf = new ShopFacade();
+//        ShopFacade sf = new ShopFacade();
 //        Shop shop = new Shop("mainShop", "mainShopAddress", 1234, 10);
 //        Shop shop = new Shop("mainShop2", "mainShopAddress2", 1234, 15);
 //        Shop shop = new Shop("mainShop3", "mainAddressShop", 4321, 5);
 //        System.out.println("sf " + sf.addShop(shop));
-
+//
 //        System.out.println("Find by zip " + sf.getShopsByPostalCode(1234));
         
     }
