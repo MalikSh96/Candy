81581832a7e6d1c9c5c79b5a3ecad8dea7899265
Jonas Elbro <cph-je98@cphbusiness.dk>
2018-11-28 10:40:51 +0100

    fix rest

--- src/main/java/rest/CandyResource.java
+++ src/main/java/rest/CandyResource.java
@@ -7,13 +7,10 @@ package rest;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import dto.CandyInfo;
 import entity.CandyType;
-import entity.Shop;
 import exceptions.NotFoundException;
 import facade.CandyFacade;
-import facade.ShopFacade;
-import java.io.IOException;
-import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Callable;
@@ -25,17 +22,11 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
-import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PUT;
 import javax.ws.rs.PathParam;
-import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import mappers.CandyInfo;
-import mappers.ShopInfo;
 
 /**
  * REST Web Service
